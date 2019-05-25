661f2648d6ec09612e7a6645ed871d093cba24f6
mukesh <sinsinwar.mukesh84@gmail.com>
2019-05-26 02:51:48 +0530

    Last commit test

--- app/src/main/java/com/farmhouse/onlinechiken/activity/SignupActivity.java
+++ app/src/main/java/com/farmhouse/onlinechiken/activity/SignupActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import com.farmhouse.onlinechiken.R;
 
 public class SignupActivity extends AppCompatActivity {
-    String s =  "learning git how it will work from android";
+    String s =  "learning git how it will work from android developer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
