6c06672db0a1198676822093dde63b924abf0233
mukesh <sinsinwar.mukesh84@gmail.com>
2019-05-26 02:19:22 +0530

    Initial commit

---
+++ app/src/main/java/com/farmhouse/onlinechiken/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.farmhouse.onlinechiken.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.farmhouse.onlinechiken.R;
+
+public class HomeActivity extends AppCompatActivity {
+    String s = "hello";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/farmhouse/onlinechiken/activity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.farmhouse.onlinechiken.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.farmhouse.onlinechiken.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
