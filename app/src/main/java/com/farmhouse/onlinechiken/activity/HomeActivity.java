a811712ad4312839f04c6f3ab75fdf78f34fac35
mukesh <sinsinwar.mukesh84@gmail.com>
2019-05-26 02:30:13 +0530

    second commit

--- app/src/main/java/com/farmhouse/onlinechiken/activity/HomeActivity.java
+++ app/src/main/java/com/farmhouse/onlinechiken/activity/HomeActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import com.farmhouse.onlinechiken.R;
 
 public class HomeActivity extends AppCompatActivity {
-    String s = "hello";
+    String s= "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
